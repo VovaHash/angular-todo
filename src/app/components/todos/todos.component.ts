d2c7c405020c02d03e74d0e8c5919cc4141e2ab9
VovaHash <vovahash01@gmail.com>
2022-08-29 23:25:08 +0300

    upd

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../models/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -7,13 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  title = "TodoComp";
+  todos:Todo[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        content :"First Todo",
+        completed : false
+      },
+      {
+        content :"Second Todo",
+        completed : false
+      },
+      {
+        content :"Third Todo",
+        completed : false
+      }
+    ]
 
+  }
 
+  toggleDone(id:number) : void{     
+    this.todos.map((v,i)=> {
+      if (i == id) v.completed = !v.completed;
+    })
   }
 
 }
